6349c295bad021fa0e797aa68738b6a07c8226e4
Michael Chen <mzc2fd@virginia.edu>
2017-03-18 21:48:09 -0700

    adding shitty but proper month support

--- app.py
+++ app.py
@@ -374,6 +374,22 @@ def flights(destination, persona, month):
     elif destination == 'ZURICH':
         dest = 'ZRH'
 
+    month = int(month)
+    month = {
+        1: '01',
+        2: '02',
+        3: '03',
+        4: '04',
+        5: '05',
+        6: '06',
+        7: '07',
+        8: '08',
+        9: '09',
+        10: '10',
+        11: '11',
+        12: '12',
+    }.get(month, '05')
+
     dep_date = '2017-' + '05' + '-01'
     ret_date = '2017-' + '05' + '-07'
     num_results = '3'
