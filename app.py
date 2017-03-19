017d7d28e3f3b9a8572ddf5cb2ae40b3691f844f
Michael Chen <mzc2fd@virginia.edu>
2017-03-18 21:31:57 -0700

    adding cors support

--- app.py
+++ app.py
@@ -14,6 +14,52 @@ from flask import Flask, g, url_for, redirect, request, make_response, send_from
 # Flask app should start in global layout
 app = Flask(__name__, static_url_path='')
 
+from datetime import timedelta
+from flask import make_response, request, current_app
+from functools import update_wrapper
+
+
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
+
 # Database config & private methods
 
 DATABASE = 'database.db'
@@ -58,6 +104,7 @@ def login():
     # return redirect('https://simulator-api.db.com/gw/oidc/authorize?response_type=code&client_id=88dfa85a-eb83-43c5-a739-47baf8234c15&state=http://localhost:5000/callback&redirect_uri=http://localhost:5000/callback', code=302)
 
 @app.route('/callback')
+@crossdomain(origin='*')
 def auth_callback():
     print('callback')
     print(request)
@@ -406,4 +453,3 @@ if __name__ == '__main__':
     url_for('static', filename='app.js')
     url_for('static', filename='common.js')
     url_for('static', filename='app.css')
-    
